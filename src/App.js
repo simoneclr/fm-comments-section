508a59d3ba2fed23e16c5f8561f279e22c0d1456
simoneclr <simone.clclr@gmail.com>
2022-03-28 15:24:45 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import CommentsDashboard from './views/comments/CommentsDashboard';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='app'>
       <CommentsDashboard/>
+
+      <Footer/>
     </div>
 
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Displays the footer
+function Footer() {
+	return (
+		<footer>
+			<div class="attribution">
+				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+				Coded by <a href="https://github.com/simoneclr" target="_blank">Simone Calciolari</a>.
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
