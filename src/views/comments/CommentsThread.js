aabb1e5a0c6a29d3c4c3e3e7d302c2c9eab9884d
simoneclr <simone.clclr@gmail.com>
2022-03-29 18:12:47 +0200

    Remove unused import

--- src/views/comments/CommentsThread.js
+++ src/views/comments/CommentsThread.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { selectCommentById, selectSortedRepliesToId } from "../../store/comments/commentsSlice";
+import { selectSortedRepliesToId } from "../../store/comments/commentsSlice";
 
 import CommentCard from "./CommentCard";
 
