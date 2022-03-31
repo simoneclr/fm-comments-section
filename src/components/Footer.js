1a0bda07bff1e1384f471068753f80f6666a0116
simoneclr <simone.clclr@gmail.com>
2022-03-31 15:11:27 +0200

    Fix vulnerability in Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,12 @@ function Footer() {
 	return (
 		<footer>
 			<div className="attribution">
-				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-				Coded by <a href="https://github.com/simoneclr" target="_blank">Simone Calciolari</a>.
+				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+					Frontend Mentor
+				</a>.
+				Coded by <a href="https://github.com/simoneclr" target="_blank" rel="noreferrer">
+					Simone Calciolari
+				</a>.
 			</div>
 		</footer>
 	)
